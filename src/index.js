7144200944ce007eaf516f27cbd32312eeea59be
mbllllb <enakes@gmail.com>
2017-10-23 02:15:13 +0300

    v. 0.0.7 heroku

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+
+const app = express();
+
+app.use(express.static('dist'));
+
+app.get('*', function (req, res) {
+  res.sendFile('index.html');
+});
+
+const port = process.env.PORT || '5000';
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('Running! (step 3b fired)'));
