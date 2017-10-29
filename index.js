e7ae2016c5ee5e48ca64eb4c2757d095931c3e74
mbllllb <enakes@gmail.com>
2017-10-29 17:06:28 +0300

    2

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ console.log('!!!!!!!!!!!!!!!!!!!!!!' + __dirname + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', function (req, res) {
+console.log('!!!!!!!!!!!!!!!!!!!!!!' + __dirname + '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+
   res.sendFile(path.join(__dirname + 'dist/index.html'));
 });
 
