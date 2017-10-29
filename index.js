2ab8445c09a8771011aec4c1701c40e873d50aed
mbllllb <enakes@gmail.com>
2017-10-29 16:57:18 +0300

    v. 0.1.5e

--- index.js
+++ index.js
@@ -4,11 +4,10 @@ const http = require('http');
 
 const app = express();
 
-app.use(express.static('dist'));
+app.use(express.static(__dirname, 'dist'));
 
-app.get('/*', function (req, res) {
-  // res.sendFile('index.html');
-  res.sendFile(path.join(__dirname + '/index.html'));
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + 'dist/index.html');
 });
 
 const port = process.env.PORT || '5000';
